ac638c6d89b7c456327913c1eb8e9ef1f8c6edf3
Ava chai <632115982@qq.com>
2021-11-21 21:47:15 +0800

    fix bug

--- libsponge/tcp_receiver.cc
+++ libsponge/tcp_receiver.cc
@@ -10,7 +10,7 @@ void DUMMY_CODE(Targs &&...  unused ) {}*/
 
 using namespace std;
 
-bool TCPReceiver::segment_received(const TCPSegment &seg) {
+bool TCPReceiver::segment_received(const TCPSegment &seg) {/*
     uint64_t old_abs_ackno = 0;
     if (read_isn) {
         old_abs_ackno = abs_ackno();
@@ -47,8 +47,50 @@ bool TCPReceiver::segment_received(const TCPSegment &seg) {
         const uint64_t absolute_seqno=unwrap(header.seqno+header.syn,isn,last_assem);   
 
     }
-    */
+    /*
+    return true;*/
+    uint64_t old_abs_ackno = 0;
+    if (isn.has_value()) {
+        old_abs_ackno = abs_ackno();
+    }
+    // Deal with the first SYN
+    if (seg.header().syn && !read_isn) {
+        isn = make_optional<WrappingInt32>(seg.header().seqno);
+    }
+
+    if (!read_isn) {
+        return false;
+    }
+
+    // absolute sequence number
+    uint64_t checkpoint = _reassembler.stream_out()
+                              .bytes_written();  // bytes_written is a little bit small, but good enough for checkpoint
+    uint64_t abs_seq = unwrap(seg.header().seqno, isn, checkpoint);
+
+
+    uint64_t old_window_size = window_size();
+
+    // ACK after FIN should be received
+    if (fin_abs_seq && abs_seq >= fin_abs_seq && seg.length_in_sequence_space() == 0) {
+        return true;
+    }
+
+    if (!(abs_seq < old_abs_ackno + old_window_size && abs_seq + seg.length_in_sequence_space() > old_abs_ackno)) {
+        // Not overlap with the window. but if it's a ack only, it's accepted.
+        return seg.length_in_sequence_space() == 0 && abs_seq == old_abs_ackno;
+    }
+    bool all_fill = abs_seq + seg.length_in_sequence_space() <= old_abs_ackno + old_window_size;
+
+    if (all_fill && seg.header().fin) {  // only when fin also fall in the window
+        fin_abs_seq = abs_seq + seg.length_in_sequence_space();
+    }
+
+    uint64_t stream_indices = abs_seq > 0 ? abs_seq - 1 : 0;
+    std::string payload(seg.payload().copy());
+    _reassembler.push_substring(payload, stream_indices, stream_indices + seg.payload().size() + 2 == fin_abs_seq);
+
     return true;
+
 }
 
 uint64_t TCPReceiver::abs_ackno()const{
