680a9b4965bb31232d71d862924cc5892ab99155
Ava chai <632115982@qq.com>
2021-11-23 15:09:31 +0800

    lab6

--- apps/webget.cc
+++ apps/webget.cc
@@ -8,7 +8,7 @@ using namespace std;
 
 void get_URL(const string &host, const string &path) {
     // Your code here.
-    CS144TCPSocket sock;
+    FullStackSocket sock;
     sock.connect(Address(host,"http"));
     // You will need to connect to the "http" service on
     // the computer whose name is in the "host" string,
